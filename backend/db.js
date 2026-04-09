735dd7895a71aea70df73933fb69e7b351186e57
atul369 <pandeyatul79465@gmail.com>
2026-04-09 21:48:46 +0530

    refactor: revert to environment variables in db.js and cleanup

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'mainline.proxy.rlwy.net',
-    user: 'root',
-    password: 'ysHjhTaXKemgPEAGGkEvXXrPeiDZyEaz',
-    database: 'railway',
-    port: 23066,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
